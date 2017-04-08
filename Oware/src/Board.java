c3a58ed976851784a129f7170d09c15c3966e48a
Shuang Zhang <shuangcool314@gmail.com>
2017-04-08 13:52:36 -0500

    Add Board functions

--- Oware/src/Board.java
+++ Oware/src/Board.java
@@ -1,22 +1,57 @@
 
 public class Board
 {
-	private static Pit[] allPits;
-	static
+	private Pit[] allPits;
+	private Board opponentBoard;
+	
+	Board()
 	{
-		allPits = new Pit[12];
-
+		allPits = new Pit[6];
+		for(int i=0; i<allPits.length; i++)
+		{
+			allPits[i].setNumSeeds(4);
+		}
 	}
 
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	public static Pit getPit(int pitnum)
+	public Pit getPit(int pitnum)
 	{
 		return allPits[pitnum];
 	}
 
+	public Board getOpponentBoard() {
+		return opponentBoard;
+	}
+
+	public void setOpponentBoard(Board opponentBoard) {
+		this.opponentBoard = opponentBoard;
+	}
+
+	public int getSeedTotalfromPit(int pitNum)
+	{
+		return allPits[pitNum].getNumSeeds();
+	}
+	
+	public void emptyPit(int pitNum)
+	{
+		allPits[pitNum].setNumSeeds(0);
+	}
+	
+	public boolean isPitExcist(int pitNum)
+	{
+		if(pitNum >= allPits.length || pitNum < 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	public void putSeedInPit(int numSeeds, int pitNum)
+	{
+		int newSeeds = allPits[pitNum].getNumSeeds();
+		newSeeds = newSeeds + numSeeds;
+		allPits[pitNum].setNumSeeds(newSeeds);
+	}
 }
