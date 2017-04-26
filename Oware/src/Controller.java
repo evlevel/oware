6565a4294875368dd836a428e219abc5b92e168a
Manalalmaeen <alma9199@stthomas.edu>
2017-04-26 11:02:34 -0500

    ManalAndWenjing

--- Oware/src/Controller.java
+++ Oware/src/Controller.java
@@ -5,7 +5,12 @@ public class Controller
 	private Player player0;
 	private Player player1;
 	private Player playerToMove;
-
+	private Board board;
+	
+	public Controller(Board b)
+	{
+		board = b;
+	}
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
@@ -13,12 +18,29 @@ public class Controller
 	}
 
 	private Pit active;
+		
+	
 
-	public Move makeMoveForCupNumber(int pitNumber)
+
+	public boolean makeMoveStartingAt(int pitNumber)
 	{
 		active = Board.getPit(pitNumber);
-
-		return new Move();
+		StdOut.println("move starts pit at pit number"+ pitNumber);
+		
+		int numSeeds= active.getNumSeeds();
+		StdOut.println("Remove"+ numSeeds+ "seeds");
+		
+		StdOut.println("Sowing in ccw direction...");
+		while(numSeeds>0)
+		{
+			active = active.getNext();
+			StdOut.println("Processing next pit =>"+active.getPitNumber());
+			active.sowOneSeed();
+			StdOut.println("Sowing one seed to it, giving its new count as :"+ active.getNumSeeds());
+			numSeeds--;
+		}
+		StdOut.println("All seeds sown.");
+		return true;
 	}
 
 }
