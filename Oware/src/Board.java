3ead450eb48235041d7444380e51f9f8e227509e
Manalalmaeen <alma9199@stthomas.edu>
2017-04-26 15:10:54 -0500

    ManalAndWenjing

--- Oware/src/Board.java
+++ Oware/src/Board.java
@@ -1,11 +1,21 @@
 
 public class Board
 {
-	private static Pit[] allPits;
+	private Pit[] allPits; 
 	
-	static
+	public Board()
 	{
 		allPits = new Pit[12];
+		
+		for(int i = 0; i<12; i++)
+		{
+			allPits[i]= new Pit();
+		}
+		
+		for (int i= 0; i< 12; i++){
+			allPits[i].setNext(allPits[(i+1)%12]);
+			allPits[i].setPrevious(allPits[(12+i-1)%12]);
+		}
 
 	}
 
--- Oware/src/Pit.java
+++ Oware/src/Pit.java
@@ -4,17 +4,31 @@ public class Pit
 	private int numSeeds;
 	private Pit next; 
 	private Pit previous; 
+	private int pitNumber;
 
 	public Pit()
 	{
-
+		numSeeds= 4;
+		pitNumber = pnum;
+	}
+	public void setPrevious(Pit p){
+		previous = p;
 	}
 
 	public int getNumSeeds()
 	{
 		return this.numSeeds;
 	}
-
+	public int getAllSeed(){
+		int temp = numSeeds;
+		numSeeds = 0;
+		return temp;
+		
+	}
+	public void sowOneSeed()
+	{
+		
+	}
 	public void setNumSeeds(int num)
 	{
 		this.numSeeds = num;
