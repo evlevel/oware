ec0a46e020a1fb38c9db380649861170aec8bb24
JQMa-635 <ma004732@stthomas.edu>
2017-04-29 11:17:43 -0500

    Update Oware.java

--- Oware/src/Oware.java
+++ Oware/src/Oware.java
@@ -1,16 +1,13 @@
 
 public class Oware
 {
-	// the GUI!
+	private static Player player;
+	private static Pit[] pits;
 	private static Board board;
-	private static Controller controller;
-	
-	/*      11 10 9 8 7 6
-	 * 		 p  p p p p p
-	 * 
-	 * 		 0  1 2 3 4 5
-	 * 		 p  p p p p p 
-	 */
+	private static boolean haveWinner;
+	private static InController choices;
+	private static Harvest doHarvest;
+	private static Move doSow;
 	
 	public static void main(String[] args)
 	{
@@ -21,13 +18,58 @@ public class Oware
 		
 		StdOut.println("Initializing...");
 		
+		initialize();
+		pits = board.getPits();
+		
+		System.out.println("            Oware Game 1.0");
+		System.out.println("-------------------------------------------\n");
+		showBoard(player, pits);
+		
+		while (!haveWinner)
+		{
+			int pitChoice = choices.chooseHouse(player, pits);
+			
+			doHarvest.harvest(doSow.move(pitChoice, pits), player);
+			showBoard(player, pits);
+			player.alternatePlayer();
+			
+			/*
+			 * Insert here.
+			 */
+			
+			if (player.getPlayerStorehouse() > 24 || player.getOpponentStorehouse() > 24)
+				haveWinner = true;
+		}
+		
+		System.out.println("-----------------------------");
+		System.out.println("     We have a WINNER!");
+		System.out.println("-----------------------------");
+		System.out.println("Thank you for playing.");
+		
+		
+	}
+	
+	private static void initialize()
+	{
+		player = new Player();
 		board = new Board();
-		controller = new Controller(board);
-		
-		StdOut.println("make a move with pit 0");
-		
-		controller.makeMoveStartingAt(0);
-		
+		haveWinner = false;
+		choices = new InController();
+		doHarvest = new Harvest();
+		doSow = new Move();
+	}
+	
+	public static void showBoard(Player p, Pit[] pits)
+	{
+		System.out.println("       12    11   10   9    8    7");
+		System.out.print("       ");
+		for (int i = 11;i > 5; i-- )
+			System.out.print("(" + pits[i].getNumSeeds() + ")  ");
+		System.out.println("\n(" + p.getPlayerStorehouse() + ")" +"--------------------------------------" + "(" + p.getOpponentStorehouse() + ")");
+		System.out.print("       ");
+		for (int i = 0;i < 6; i++ )
+			System.out.print("(" + pits[i].getNumSeeds() + ")  ");
+		System.out.println("\n        1    2    3    4    5    6");
 	}
 
 }
