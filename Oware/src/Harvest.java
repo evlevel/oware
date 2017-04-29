72efdfa3fab2979234d696da4fda00ef591415b9
JQMa-635 <ma004732@stthomas.edu>
2017-04-29 11:10:22 -0500

    Update Harvest.java

--- Oware/src/Harvest.java
+++ Oware/src/Harvest.java
@@ -1,1 +1,50 @@
-
+class Harvest
+{
+	public void harvest(Pit house, Player player, House[] houses)
+	{
+		if (player.getWhichPlayer())
+		{
+			while (true)
+			{
+				int seedsInPit = pitToEmpty.getNumSeeds();
+				if (pitToEmpty.getPitNumber() >= 6 && pitToEmpty.getPitNumber() <= 11) // The last sowed pit is in the "harvestable" pit
+				{
+          
+					if (seedsInPit == 2 || seedsInPit == 3)
+					{
+						player.addSeedsToPlayerStorehouse(seedsInPit);
+						pitToEmpty.emptyPit();
+            pitToEmpty = pitToEmpty.getPrevious();
+					}
+					else
+						break;
+				}
+        
+				else
+					break;
+			}				
+		}
+		else
+    {
+			while (true)
+			{
+				int seedsInPit = pitToEmpty.getNumSeeds();
+				if (pitToEmpty.getPitNumber() >= 0 && pitToEmpty.getPitNumber() <= 5) // The last sowed pit is in the "harvestable" pit
+				{
+          
+					if (seedsInPit == 2 || seedsInPit == 3)
+					{
+						player.addSeedsToOppontStorehouse(seedsInPit);
+						pitToEmpty.emptyPit();
+            pitToEmpty = pitToEmpty.getPrevious();
+					}
+        
+				  else
+					  break;
+        }
+				else
+					break;
+			}				
+	  }
+  }
+}
