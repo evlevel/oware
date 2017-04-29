40ed833e560b75ff8a935edb8802e81dcef4be06
JQMa-635 <ma004732@stthomas.edu>
2017-04-29 10:40:28 -0500

    Update InController.java

--- Oware/src/InController.java
+++ Oware/src/InController.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class InController
 {
-	public int chooseHouse(Player player, House[] houses) 
+	public int chooseHouse(Player player, Pit[] pits) throws Exception // Catching a input other than a number between 1 - 12
 	{
 		Scanner input = new Scanner(System.in);
 		int choice = 0;
@@ -31,7 +31,7 @@ class InController
 				}
 				if (choice < 1 || choice > 6)
 					System.out.print("Invalid input. Enter again. ");
-				else if (houses[choice-1].getSeedsCount() == 0)
+				else if (pits[choice-1].getNumSeeds() == 0)
 					System.out.print("House already empty. Enter again. ");
 				else
 					break;
@@ -42,7 +42,7 @@ class InController
 			while (true)
 			{
 				System.out.print("\nPlease a house from your side(7 - 12): ");
-				while (true)
+				while (true) //Catching
 				{
 					try
 					{
@@ -58,13 +58,13 @@ class InController
 				}
 				if (choice < 6 || choice > 12)
 					System.out.print("Invalid input. Enter again. ");	
-				else if (houses[choice-1].getSeedsCount() == 0)
+				else if (pits[choice-1].getNumSeeds() == 0)
 					System.out.print("House already empty. Enter again. ");
 				else
 					break;
 			}
 		}
 		
-		return choice - 1;
+		return choice - 1; //Since we using an array for pits
 	}
 }
