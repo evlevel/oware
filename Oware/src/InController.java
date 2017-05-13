00f1ce3e14613d9b0c6fb38cca019e0c86cadda7
JQMa-635 <ma004732@stthomas.edu>
2017-05-13 14:42:05 -0500

    Update InController.java
    Get rid of repeated code.

--- Oware/src/InController.java
+++ Oware/src/InController.java
@@ -8,11 +8,10 @@ class InController
 		Scanner input = new Scanner(System.in);
 		int choice = 0;
 		
-		if (player.getWhichPlayer()) // For player choosing from pits 1 - 6
-		{
+
 			while (true)
 			{
-				System.out.print("\nPlease a house from your side(1 - 6): ");
+				showPitToSelect(player);
 				while (true)
 				{
 					try
@@ -22,49 +21,48 @@ class InController
 					catch (InputMismatchException e)
 					{
 						System.out.print("Invalid input. Enter again. ");
-						System.out.print("\nPlease a house from your side(1 - 6): ");
+						showPitToSelect(player);
 						input.next();
 					}
 
 					if (choice != 0)
 						break;
 				}
-				if (choice < 1 || choice > 6)
+				if (ifPitIsChoosable(choice, player))
 					System.out.print("Invalid input. Enter again. ");
 				else if (pits[choice-1].getNumSeeds() == 0)
 					System.out.print("House already empty. Enter again. ");
 				else
 					break;
 			}
-		}
-		else // For player choosing from pits 7 - 12
-		{
-			while (true)
-			{
-				System.out.print("\nPlease a house from your side(7 - 12): ");
-				while (true) //Catching
-				{
-					try
-					{
-						choice = input.nextInt();
-					}
-					catch (InputMismatchException e)
-					{
-						System.out.print("Invalid input. Enter again. ");	
-						input.next();
-					}
-					if (choice != 0)
-						break;
-				}
-				if (choice < 6 || choice > 12)
-					System.out.print("Invalid input. Enter again. ");	
-				else if (pits[choice-1].getNumSeeds() == 0)
-					System.out.print("House already empty. Enter again. ");
-				else
-					break;
-			}
-		}
 		
 		return choice - 1; //Since we using an array for pits
 	}
+	
+	private void showPitToSelect(Player player)
+	{
+		if (player.getWhichSide())
+			System.out.print("\nPlease a house from your side(1 - 6): ");
+		else
+			System.out.print("\nPlease a house from your side(7 - 12): ");
+	}
+	
+	private boolean ifPitIsChoosable(int choice, Player player)
+	{
+		if (player.getWhichSide())
+		{
+			if (choice >= 7 && choice <= 11)
+				return true;
+			else 
+				return false;
+		}
+		else
+		{
+			if (choice >= 1 && choice <= 6)
+				return true;
+			else
+				return false;	
+		}
+	}
+	
 }
